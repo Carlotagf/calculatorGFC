f777fcb531875ccae05321e61b9560d6a4eb3d9d
Carlotagf <carlotagf00@gmail.com>
2026-05-11 19:39:50 +0200

    Checkstyle

--- src/main/java/com/dam/calculatorGFC/CalculatorGfcApplication.java
+++ src/main/java/com/dam/calculatorGFC/CalculatorGfcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorGfcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorGfcApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(CalculatorGfcApplication.class, args);
+}
 
 }
