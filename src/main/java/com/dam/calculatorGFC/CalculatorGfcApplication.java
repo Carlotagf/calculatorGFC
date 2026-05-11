56b8502ba2dd048698a9c1f4e74fe3c70e327b87
Carlotagf <carlotagf00@gmail.com>
2026-05-11 17:22:57 +0200

    Initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/main/java/com/dam/calculatorGFC/CalculatorGfcApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorGFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/calculatorGFC/CalculatorGfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorGFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
